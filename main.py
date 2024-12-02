95622f96f125287a3a99615c4d3959f26b39cc1a
Admin@DESKTOP-EQ55Q8H <amina.hamzic@stu.ibu.edu.ba>
2024-12-02 21:00:57 +0100

    Project structure

---
+++ main.py
@@ -0,0 +1,6 @@
+# Research question: Can the integration of collaborative and content-based
+# filtering techniques overcome the limitations of standalone recommendation approaches in personalized news systems?
+
+
+# Importing the required libraries
+import pandas as pd
